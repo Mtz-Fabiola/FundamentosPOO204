1a6164f21977bf69af3068d6e9f40a13baa07b8a
Mtz-Fabiola <fm0560240@gmail.com>
2025-01-22 15:35:03 -0600

    Finalizamos practica No2 Sintaxis

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,15 +1,93 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello, Fabiola Mtz R !");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//IMPORTACIONES
+//import java.util.Random;
+import java.util.Scanner;
+
+//CLASE PRINCIPAL
+public class Main {
+
+    //Metodo Principal (Main)
+    public static void main(String[] args) {
+
+//        System.out.printf("Hello, Fabiola Mtz R !");
+//        String cadenas = "fabiola" + "martinez" + "rauda";
+//
+//        System.out.println(cadenas);
+//
+//        System.out.println(cadenas.length());
+//
+//        System.out.println(cadenas.substring(2,5));
+//        System.out.println(cadenas.substring(2));
+//        System.out.println(cadenas.substring(0,5));
+
+//   3. Variables
+//        int x=5,x2;
+//        double y = 1.25,y2;
+//        String z = "204",z1;
+//
+//        //Conversion de cadena a int
+//        x2=Integer.parseInt(z);
+//
+//        //Conversion de double a cadena
+//        z1= String.valueOf(y);
+//
+//        //Conversion de int a double
+//        double asd = Double.valueOf(x);
+//
+//        //Conversion Implicita
+//        int num = 12;
+//        double numD = num;
+//        System.out.println("Conversion Implicita"+numD);
+//
+//
+//        System.out.println(x2);
+//        System.out.println(z1);
+//        System.out.println(x);
+//
+//        //Creamos un objeto rdn de clase Random
+//        Random rdn = new Random();
+//
+//        //Generamos numeros y los guardamos
+//        int numAleatorio = rdn.nextInt();
+//        double numDouble = rdn.nextDouble();
+//
+//        System.out.println("Aleatorio entero: "+numAleatorio);
+//        System.out.println("Aleatorio double: "+numDouble);
+
+        //4. Solicitud de datos
+
+
+//        Scanner scn = new Scanner(System.in);
+//
+//        System.out.println("Introduce cualquier dato ");
+//        String dato = scn.nextLine();
+//
+//        System.out.println("Introduce dato entero");
+//        int datoentero = scn.nextInt();
+//
+//        System.out.println("Introduce dato decimal ");
+//        double datodecimal = scn.nextDouble();
+//
+//        System.out.println("Cualquier dato" + dato);
+//        System.out.println("Dato Entero" + datoentero);
+//        System.out.println("Dato Decimal" + datodecimal);
+
+
+        //5.Boolean, operadores logicos y de comparacion
+
+        System.out.println(10>9); //True
+        System.out.println(10==9); //False
+        System.out.println(10<9); //False
+        System.out.println(10>=9); //True
+        System.out.println(10<=9); //False
+        System.out.println(10!=9); //True
+
+
+        int x = 3;
+        System.out.println(x< 5 && x>10);//False  se tienen que cumplir las dos
+        System.out.println(x< 5 || x>10);//True  aunque se cumpla una
+        System.out.println(!(x< 5 && x>10));//True  Se niegan ambas
+
+
         }
     }
-}
