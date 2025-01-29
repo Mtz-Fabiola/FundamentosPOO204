3cdf7db19274a13fead0834df6d78e8da1e4bc35
Mtz-Fabiola <fm0560240@gmail.com>
2025-01-28 23:00:02 -0600

    Ejercicio 3 Practica 3 finalizado

---
+++ ejercicio 3 Practica 3/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un número entero : ");
+        int X = scanner.nextInt();
+
+        int suma = 0;
+        for (int i = 1; i <= X; i++) {
+            suma += i;
+        }
+
+        System.out.println("La suma de todos los enteros desde 1 hasta " + X + " es: " + suma);
+
+    }
+}
